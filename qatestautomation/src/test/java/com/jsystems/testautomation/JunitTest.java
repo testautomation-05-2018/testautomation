b7cfa0240ff8e86dd9b9ee20b7bf6f7c2253b7c9
testautomation <testautomation052018@wp.pl>
2018-05-21 15:49:23 +0200

    created firstTest, created and set pom.xml

--- qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
+++ qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
@@ -2,13 +2,20 @@ package com.jsystems.testautomation;
 
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 public class JunitTest {
 
+
+
     @Test
     public void firstTest(){
+
+        String testowyString = "firstTest";
+
         assertTrue(true == true);
+        assertTrue(testowyString.equals("firstTest"));
+
     }
 
 }
